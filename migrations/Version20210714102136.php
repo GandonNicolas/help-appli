0f80635ab6aebd6dd80548cd2b102c2cfbc6479c
Jeanne-Louise GAUDIN <gaudin.jeannelouise@gmail.com>
2021-07-14 12:26:33 +0200

    modif des migrations

--- migrations/Version20210714102136.php
+++ migrations/Version20210714102136.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210607095516 extends AbstractMigration
+final class Version20210714102136 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,15 @@ final class Version20210607095516 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE keyword_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE keyword (id INT NOT NULL, label VARCHAR(20) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE SEQUENCE test_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE test (id INT NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE keyword_id_seq CASCADE');
-        $this->addSql('DROP TABLE keyword');
+        $this->addSql('DROP SEQUENCE test_id_seq CASCADE');
+        $this->addSql('DROP TABLE test');
     }
 }
